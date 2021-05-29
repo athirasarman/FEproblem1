0c4f09ee0d827f0229c1c02242b02155a6416d08
athirasarman <kaithakkottuathira@gmail.com>
2021-05-29 19:56:37 +0530

    responsive issue fix

--- FEproblem1/src/app/app-routing.module.ts
+++ FEproblem1/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DummyComponent } from './dummy/dummy.component';
 import { PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   
    { path: 'findingfalcone', loadChildren: () => import('./finding-falcone/finding-falcone.module').then(m => m.FindingFalconeModule) },
    { path: '', redirectTo:'dashboard', pathMatch:'full'},
-   { path: 'result',redirectTo:'result',pathMatch:'full'},
+   { path: 'result',redirectTo:'result',pathMatch:'full'},  
+   { path:'DummyComponent', component:DummyComponent},   
    { path:'404', component:PageNotFoundComponent},
     {path: '**', redirectTo: '/404'}
 ];
